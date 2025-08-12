e6fc447d6257d3b2a7dec22e183c9a9a61f0e731
rnaraks-sudo <rok.naraks@2mobile.si>
2025-08-12 14:09:24 +0200

    Delete tracker-capacitor directory

--- tracker-capacitor/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'si.twomobile.tracker',
-  appName: 'Tracker+CO2',
-  webDir: 'www',
-  bundledWebRuntime: false,
-  server: { androidScheme: 'https' }
-};
-export default config;
